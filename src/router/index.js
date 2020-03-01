9724cdace4ba4658164447b440dcbdd16b168f05
Liangyunpeng <214800189@qq.com>
2020-03-01 23:09:28 +0800

    构建项目，配置全局样式及路由ElementUI

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
+
+const routes = [
+  // 重定向
+  {
+    path: "/",
+    redirect:"login"
+  },
+  {
+    path: "/login",
+    name: "login",
+    component:()=> import("../views/login/index.vue")
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
---
+++ src/views/login/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="login">这里是登录页面</div>
+</template>
+<script>
+export default {
+    name:'login'
+};
+</script>
+<style lang="scss" scoped>
+#login{
+    // 可视区的高度为100%
+    height:100vh
+}
+</style>
