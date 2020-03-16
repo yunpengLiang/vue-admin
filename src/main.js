3b235ca9370757a368f527d9ec4ab8b1ed169c5b
Liangyunpeng <214800189@qq.com>
2020-03-16 09:51:37 +0800

    将vue2.0改为3.0

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import Vue from "vue";
+import VueCompositionApi from '@vue/composition-api'; 
 import App from "./App.vue";
 import router from "./router/index.js";
 import store from "./store";
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
-
-Vue.config.productionTip = false;
+Vue.use(VueCompositionApi);
 Vue.use(ElementUI);
+Vue.config.productionTip = false;
 new Vue({
   router,
   store,
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,7 +4,7 @@
         <!-- 内容区 -->
         <div class="login-warp">
             <ul class="menu-tab">
-                <li v-for="item in menuTab" :key="item.id" :class="{'current':item.current}" @click="toggleMenu(item)" >{{item.txt}}</li>
+                <li v-for="(item,index) in menuTab" :key="item.id" :class="{'current':item.current}" @click="toggleMenu(item,index)" >{{item.txt}}</li>
             </ul>
             <!-- 表单的start -->
             <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm"  class="login-form" size="medium">
@@ -38,20 +38,22 @@
                 </el-form-item>
 
                 <el-form-item>
-                    <el-button type="danger" class="login-btn block"   @click="submitForm('ruleForm')">{{menuTab[0].txt}}</el-button>
+                    <el-button type="danger" class="login-btn block"   @click="submitForm('ruleForm')">{{menuTab[indexnum].txt}}</el-button>
                 </el-form-item>
             </el-form>
         </div>
     </div>
 </template>
 <script>
+import {reactive,ref, isRef, toRefs, onMounted} from "@vue/composition-api";
 import {stripscript,validateEmail,validatePass,validateCheckCode} from "@/utils/validate";
 export default {
     // 当前模块名称
     name:'login',
-    data(){
+    // setup(props,context){
+     setup(props,{refs}){
         // 验证验证码
-        var checkCode = (rule, value, callback) => {
+        let checkCode = (rule, value, callback) => {
             if (value === '') {
                 callback(new Error('请输入验证码'));
             } else if (validateCheckCode(value)) {
@@ -61,7 +63,7 @@ export default {
             }
         };
         // 验证用户名邮箱
-        var validateUsername = (rule, value, callback) => {
+        let validateUsername = (rule, value, callback) => {
             if (value === '') {
                 callback(new Error('请输入用户名'));
             } else if(validateEmail(value)){
@@ -71,10 +73,10 @@ export default {
             }
         };
         // 验证密码
-        var validatePassword = (rule, value, callback) => {
+        let validatePassword = (rule, value, callback) => {
             // 当前密码框的值等于过滤后的数据
-            this.ruleForm.password = stripscript(value);
-            value = this.ruleForm.password;
+            ruleForm.password = stripscript(value);
+            value = ruleForm.password;
             if (value === '') {
                 callback(new Error('请输入密码'));
             } else if (validatePass(value)) {
@@ -84,65 +86,76 @@ export default {
             }
         };
         // 验证重复密码
-        var validatePasswords = (rule, value, callback) => {
+        let validatePasswords = (rule, value, callback) => {
             // 当前密码框的值等于过滤后的数据
-            this.ruleForm.passwords = stripscript(value);
-            value = this.ruleForm.passwords;
+            ruleForm.passwords = stripscript(value);
+            value = ruleForm.passwords;
             if (value === '') {
                 callback(new Error('请再次输入密码'));
-            } else if (value != this.ruleForm.password) {
+            } else if (value != ruleForm.password) {
                 callback(new Error('重复密码不正确!'));
             } else {
                 callback();
             }
         };
-        return{
-            menuTab:[
-                {txt:'登录',current:true,type:'login'},
-                {txt:'注册',current:false,type:'register'}
+
+        // 这里放置data数据，生命周期，自定义的函数，对象类型使用reactive来声明
+        const menuTab = reactive([
+            {txt:'登录',current:true,type:'login'},
+            {txt:'注册',current:false,type:'register'},
+            
+        ]);
+        console.log(menuTab);
+        // 声明数据类型为基础数据类型,取值的话需要使用变量名.value
+        const model = ref('login');
+        const indexnum = ref('0');
+        const ruleForm = reactive({
+            username: '',
+            password: '',
+            passwords: '',
+            code: ''
+        })
+        // 表单的验证
+        const  rules = reactive({
+            username: [
+                { validator: validateUsername, trigger: 'blur' }
             ],
-            // 模块的值
-            model:'login',
-            // isActive:true
-            ruleForm: {
-                username: '',
-                password: '',
-                passwords: '',
-                code: ''
-            },
-            rules: {
-                username: [
-                    { validator: validateUsername, trigger: 'blur' }
-                ],
-                password: [
-                    { validator: validatePassword, trigger: 'blur' }
-                ],
-                passwords: [
-                    { validator: validatePasswords, trigger: 'blur' }
-                ],
-                code: [
-                    { validator: checkCode, trigger: 'blur' }
-                ]
-            }
-        }
-    },
-    created(){},
-    mounted(){
-        
-    },
-    methods:{
+            password: [
+                { validator: validatePassword, trigger: 'blur' }
+            ],
+            passwords: [
+                { validator: validatePasswords, trigger: 'blur' }
+            ],
+            code: [
+                { validator: checkCode, trigger: 'blur' }
+            ]
+        });
+        console.log(model.value);
+        // model.value = 100   赋值
+        // isRef(model),判断model是不是基础数据类型
+        console.log(isRef(model) ? true : false)
+
+        console.log(toRefs(menuTab));
+        console.log(menuTab);
+        // 生命周期，挂载完成之后执行
+        onMounted(()=>{
+
+        })
+        // 声明函数
         // 数据驱动视图
-        toggleMenu(data){
-            this.menuTab.forEach((elem,index) => {
+        const toggleMenu = ((data,index)=>{
+            indexnum.value = index ;
+            menuTab.forEach((elem,index) => {
                 elem.current = false;
             });
+           
             // 高光
             data.current=true;
             // 修改模块的值
-            this.model = data.type;
-        },
-        submitForm(formName) {
-            this.$refs[formName].validate((valid) => {
+            model.value = data.type;
+        })
+        const submitForm = (formName=> {
+            refs[formName].validate((valid) => {
                 if (valid) {
                     alert('submit!');
                 } else {
@@ -150,11 +163,23 @@ export default {
                     return false;
                 }
             });
-        },
-        resetForm(formName) {
-            this.$refs[formName].resetFields();
+        })
+        const resetForm = (formName=> {
+            refs[formName].resetFields();
+        })
+        return{
+            menuTab,
+            model,
+            indexnum,
+            ruleForm,
+            rules,
+            toggleMenu,
+            submitForm,
+            resetForm
         }
     },
+    created(){},
+    
     props:{},
     watch:{}
 };
